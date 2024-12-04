164e7e1a5c89c3e27185b401a48cee5a5bbbe2f2
IlyaZabrodin <ilyazabrodin.28@mail.ru>
2024-12-04 18:13:29 +0300

    Добавлена работа с корзиной; Сделано вторичное составление заказа

---
+++ main_app/dish_list/model_route.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from flask import session
+
+from database.operations import insert_one
+from database.operations import update
+from pymysql import OperationalError
+
+
+@dataclass
+class ProductInfoRespronse:
+    result: tuple
+    error_message: str
+    status: bool
+
+
+def model_route_transaction_order(db_config: dict, sql_provider, basket: dict, price: int, order_id: int):
+    total_amount = 0
+    for key, value in basket.items():
+        _sql = sql_provider.get('insert_dish_list.sql',
+                                dict(dish_id=int(key), order_id=order_id, dish_amount=int(value)))
+        result = insert_one(db_config, _sql)
+        total_amount += int(value)
+        if not result:
+            raise OperationalError("Dish is not appended")
+    _sql = sql_provider.get('update_order.sql', dict(order_id=order_id, prepaid_expense=price,
+                                                     dish_amount=total_amount, order_status="Полностью оформлен"))
+    res = update(db_config, _sql)
+    print(res)
+    if not res:
+        raise OperationalError("Order list is not updated")
+
+    result = tuple(str(order_id))
+    return ProductInfoRespronse(result, error_message="", status=True)
